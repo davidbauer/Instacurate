8b0d6ee77afdb1fc3f011ec7491b5c1e5ee11070
Thomas Jaggi <thomas.jaggi@gmail.com>
2013-01-04 14:54:42 +0100

    Avoid `console` errors in browsers that lack a console

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// Avoid `console` errors in browsers that lack a console
+// https://github.com/h5bp/html5-boilerplate/blob/master/js/plugins.js
+(function() {
+    var method,
+        noop = function() {},
+        methods = [
+            'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+            'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+            'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+            'timeStamp', 'trace', 'warn'
+        ],
+        length = methods.length,
+        console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+
+
 // Setup an event listener for the form that will execute checkUser()
 // when the form is submitted.
 $(function() {
